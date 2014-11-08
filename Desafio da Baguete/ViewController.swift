c0a9325f477dddd4e55e3d2d0ddb795ed4e5e0d9
João Bernardo <jb.amaro@hotmail.com>
2014-11-08 21:03:22 +0000

    Initial Commit

---
+++ Desafio da Baguete/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Desafio da Baguete
+//
+//  Created by João Bernardo on 08/11/14.
+//  Copyright (c) 2014 jeKnowledge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
